6ce44b4ce898259eaf9f843f2960a55dc90a51c6
hihi188 <96903776+hihi188@users.noreply.github.com>
2021-12-31 14:21:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    titleWeb: "WelcomeTo2022",
+    introTitle: 'Haloooo Mẹppppp 🥰',
+    introDesc: ` Cám ơn đã làm việc chăm chỉ, đã mạnh mẽ cố gắng suốt năm qua~ 
+2021 có gì mịt mỏi pùn phiền gì thì bỏ lại ùi cùm nhau sang năm mới thôuuu🎉`,
+    btnIntro: '^^LetsGoooo^^',
+    title: '2021 của Mẹp như thế nàoo?🥺',
+    desc: 'Điều gì trong năm 2021 tuỵt nhất đem lại choa Mẹp dzaa??🤓',
+    btnYes: 'Là NH chứ còn gì nữaaaa<33',
+    btnNo: 'Không có gì đặc biệt :3',
+    question: ' Nào nhắmm mắt 3 giây suy nghĩ òyy ghi những đìu ước mong muốn cho 2022 nàaaa 🧚🏻‍♀️💕',
+    btnReply: '🌟Send It🍀',
+    reply: 'Dù hong bít B đã ước gì nhưng mong có thể cố gắng và thực hiện nó cùm nhau^^',
+    mess: 'TengTengg🥳 Thế là baibai 2021 ùii👋 ',
+    messDesc: ' Mới đây mừ nch lại vs b hơn nửa năm òyy~🤭 Nhanh vaizz^^ Trch chưa baoh nghĩ là nch, gặp lại b vậy mà bâyh nó dãy nè:)))) quả là hong gì đoán trc đc nhuôn mèee🤣
+    2021 khom gì đặc biệt lớm bùn zui gì cũm coáa~ Chắc đìu tuỵt zời đág nhớ nhất nem nay của mềnh là b ồuu👉🏻👈🏻Dù hay hờn zỗi nhưng cũm mún nói Kám mơn b lại xuất hiện thêm lần nữaaa🤍 
+    2022 gồu sẽ coáa nhìu nìm zui hơn🍀🤩 Qua năm mới có gì cực khổ mịt mỏi thì đừng gánh mụt mình, luôn có mình ây mừ~~😘 2022 có mong ước gì thì cố gắng làm vs nhau ùi từ từ sẽ đạt được💪🏼 
+    Chúc b sớm có đc mình nàaa 👻^^ Cũng mong mũi ngày của 2022 sẽ thức hiện được nhiều điều ý nghĩa cùm nhau hơn nhannnnnnnn❣️Xoxo </3 🌟
+    Luvvv from NH ♥️',
+    btnAccept: '♥️',
